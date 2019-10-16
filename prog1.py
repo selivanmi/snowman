646250e1b36d69ce9c530777982300f213afca77
Mikhail Seliverstov <minisel@gmail.com>
2019-10-17 00:03:04 +0300

    Revert "Update prog1.py"
    This reverts commit 0c175e5be800507a2110b7a571d897c20ad9bb83.

--- prog1.py
+++ prog1.py
@@ -1,14 +1,9 @@
 str = "mother cleans the window"
 
 def process_string(str):
-    """
-    This function accepts a sentence and return list of words it is made of.
-    @param str: Text string to process
-    @return: List of words
-    """
     result = []
-    alphaIdx = -1  # Value -1 means that the current character is not a letter
-    i = 0          # Index of the current character
+    alphaIdx = -1
+    i = 0
     for char in str:
         if char.isalpha():
             if alphaIdx < 0:
