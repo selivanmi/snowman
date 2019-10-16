c956bf2d8cbb5fcc86a2ef4cc85da0c302ddf48b
selivanmi <56321315+selivanmi@users.noreply.github.com>
2019-10-17 00:06:47 +0300

    Update prog1.py
    Added documentation

--- prog1.py
+++ prog1.py
@@ -1,9 +1,14 @@
 str = "mother cleans the window"
 
 def process_string(str):
+    """
+    This function accepts a sentence and return list of words it is made of.
+    @param str: Text string to process
+    @return: List of words
+    """
     result = []
-    alphaIdx = -1
-    i = 0
+    alphaIdx = -1  # Value -1 means that the current character is not a letter
+    i = 0          # Index of the current character
     for char in str:
         if char.isalpha():
             if alphaIdx < 0:
