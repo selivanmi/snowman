3fd666a32b98341a0c8fdf75dbf328c964817eed
selivanmi <56321315+selivanmi@users.noreply.github.com>
2019-11-07 01:10:38 +0300

    class created

--- testTokenization.py
+++ testTokenization.py
@@ -5,23 +5,26 @@ class TestStringMethods(unittest.TestCase):
 
   def test_mother_string(self):
       s = 'mother cleans the window'
-      self.assertEqual(string_to_alpha.process_string(s), ['mother', 'cleans', 'the', 'window'])
+      self.assertEqual(string_to_alpha.StringTokenizer.process_string(s),
+                       ['mother', 'cleans', 'the', 'window'])
 
   def test_empty_string(self):
       s = ''
-      self.assertEqual(string_to_alpha.process_string(s), [])
+      self.assertEqual(string_to_alpha.StringTokenizer.process_string(s), [])
 
   def test_no_spaces_string(self):
       s = 'mothercleansthewindow'
-      self.assertEqual(string_to_alpha.process_string(s), ['mothercleansthewindow'])
+      self.assertEqual(string_to_alpha.StringTokenizer.process_string(s),
+                       ['mothercleansthewindow'])
 
   def test_digital_string(self):
       s = '012345'
-      self.assertEqual(string_to_alpha.process_string(s), [])
+      self.assertEqual(string_to_alpha.StringTokenizer.process_string(s), [])
 
   def test_delimiters_string(self):
       s = 'mother_cleans+the-window'
-      self.assertEqual(string_to_alpha.process_string(s), ['mother', 'cleans', 'the', 'window'])
+      self.assertEqual(string_to_alpha.StringTokenizer.process_string(s),
+                       ['mother', 'cleans', 'the', 'window'])
         
 if __name__ == '__main__':
     unittest.main()
