6bdf5ef5fef0fcf6a5cf5964414952f292c8df66
selivanmi <56321315+selivanmi@users.noreply.github.com>
2019-11-12 21:44:42 +0300

    added a class and changed unittest

---
+++ string_to_alpha0.py
@@ -0,0 +1,27 @@
+"""This module provides a function that tokenizes the given text string"""
+
+def process_string(str):
+    """
+    This function accepts a text string 
+    and returns a list of words it consists of.
+    Word is a text string which consists only of letters.
+    @param str: Text string to process
+    @return: A list of words
+    """
+    result = []
+    alphaIdx = -1  # Value -1 means that the current character is not a letter
+    i = 0          # Index of the current character
+    for char in str:
+        if char.isalpha():
+            if alphaIdx < 0:
+                alphaIdx = i
+        else:
+            if alphaIdx > -1:
+                result.append(str[alphaIdx:i])
+                alphaIdx = -1
+        i += 1
+    if alphaIdx > -1:
+        # We check if the last character is a letter
+        # If it is true, we add the substring to the list of words
+        result.append(str[alphaIdx:i])
+    return result
---
+++ string_to_alpha1.py
@@ -0,0 +1,8 @@
+"""This module provides a function that tokenizes the given text string"""
+import string_to_alpha0
+
+class StringTokenizer:
+    """This class tokenizes the string using the method 'process_string'"""
+    def process_string(str):
+        return string_to_alpha0.process_string(str)
+        
