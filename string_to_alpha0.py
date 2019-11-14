0a82a8ed566ea5c23e504863acb25d6cbf2b0996
selivanmi <56321315+selivanmi@users.noreply.github.com>
2019-11-14 10:15:03 +0300

    Add files via upload

---
+++ string_to_alpha0.py
@@ -0,0 +1,58 @@
+"""This module contains methods and a function
+that tokenizes the given text string"""
+
+def process_string(str):
+    """
+    This function accepts a text string 
+    and returns a list of words it consists of.
+    Word is a text string which consists only of letters.
+    @param str: Text string to process
+    @return: A list of words
+    """
+    result = []
+    alphaIdx = -1  # Value -1 means that the current character is not a letter
+    i = 0          # Index of the current character
+    for char in str:
+        if char.isalpha():
+            if alphaIdx < 0:
+                alphaIdx = i
+        else:
+            if alphaIdx > -1:
+                result.append(str[alphaIdx:i])
+                alphaIdx = -1
+        i += 1
+    if alphaIdx > -1:
+        # We check if the last character is a letter
+        # If it is true, we add the substring to the list of words
+        result.append(str[alphaIdx:i])
+    return result
+
+class StringTokenizer(object):
+    """This class tokenizes the string using the method 'process_string'"""
+    def tokenize(str):
+         """
+         This method accepts a text string
+         and returns a list of words it consists of.
+         Word is a text string which consists only of letters.
+         @param str: Text string to process
+         @return: A list of words
+         """
+         result = []
+         alphaIdx = -1  # Value -1 means that the current character is not a letter
+         i = 0          # Index of the current character
+         for char in str:
+             if char.isalpha():
+                 if alphaIdx < 0:
+                     alphaIdx = i
+             else:
+                 if alphaIdx > -1:
+                    result.append(str[alphaIdx:i])
+                    alphaIdx = -1
+             i += 1
+         if alphaIdx > -1:
+           # We check if the last character is a letter
+           # If it is true, we add the substring to the list of words
+           result.append(str[alphaIdx:i])
+         return result
+
+
---
+++ testTokenizator_selivanmi.py
@@ -0,0 +1,56 @@
+import unittest
+import string_to_alpha0
+
+class TestStringMethods(unittest.TestCase):
+
+  def test_mother_string(self):
+      s = 'mother cleans the window'
+      self.assertEqual(string_to_alpha0.process_string(s),
+                       ['mother', 'cleans', 'the', 'window'])
+
+  def test_empty_string(self):
+      s = ''
+      self.assertEqual(string_to_alpha0.process_string(s), [])
+
+  def test_no_spaces_string(self):
+      s = 'mothercleansthewindow'
+      self.assertEqual(string_to_alpha0.process_string(s),
+                       ['mothercleansthewindow'])
+
+  def test_digital_string(self):
+      s = '012345'
+      self.assertEqual(string_to_alpha0.process_string(s), [])
+
+  def test_delimiters_string(self):
+      s = 'mother_cleans+the-window'
+      self.assertEqual(string_to_alpha0.process_string(s),
+                       ['mother', 'cleans', 'the', 'window'])
+  
+  def test_class_mother_string(self):
+      s = 'mother cleans the window'
+      self.assertEqual(string_to_alpha0.StringTokenizer.tokenize(s),
+                       ['mother', 'cleans', 'the', 'window'])
+
+  def test_class_empty_string(self):
+      s = ''
+      self.assertEqual(string_to_alpha0.StringTokenizer.tokenize(s), [])
+
+  def test_class_no_spaces_string(self):
+      s = 'mothercleansthewindow'
+      self.assertEqual(string_to_alpha0.StringTokenizer.tokenize(s),
+                       ['mothercleansthewindow'])
+
+  def test_class_digital_string(self):
+      s = '012345'
+      self.assertEqual(string_to_alpha0.StringTokenizer.tokenize(s), [])
+
+  def test_class_delimiters_string(self):
+      s = 'mother_cleans+the-window'
+      self.assertEqual(string_to_alpha0.StringTokenizer.tokenize(s),
+                       ['mother', 'cleans', 'the', 'window'])
+        
+if __name__ == '__main__':
+    str = "mother cleans the window"
+    print(string_to_alpha0.process_string(str))
+    unittest.main()
+    
