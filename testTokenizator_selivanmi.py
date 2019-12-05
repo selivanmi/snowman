92ff52d8939c2c24b21e43e2d47571573c931d39
selivanmi <56321315+selivanmi@users.noreply.github.com>
2019-12-05 09:29:59 +0300

    Update testTokenizator_selivanmi.py

--- testTokenizator_selivanmi.py
+++ testTokenizator_selivanmi.py
@@ -10,51 +10,51 @@ class TestStringTokenizer(unittest.TestCase):
 
 
      def test_class_mother_string(self):
-      s = 'mother cleans the window'
-      self.assertEqual(self.t.tokenize(s),
+          s = 'mother cleans the window'
+          self.assertEqual(self.t.tokenize(s),
                        ['mother', 'cleans', 'the', 'window'])
 
      def test_class_empty_string(self):
-      s = ''
-      self.assertEqual(self.t.tokenize(s), [])
+          s = ''
+          self.assertEqual(self.t.tokenize(s), [])
 
      def test_class_no_spaces_string(self):
-      s = 'mothercleansthewindow'
-      self.assertEqual(self.t.tokenize(s),
+          s = 'mothercleansthewindow'
+          self.assertEqual(self.t.tokenize(s),
                        ['mothercleansthewindow'])
 
      def test_class_digital_string(self):
-      s = '012345'
-      self.assertEqual(self.t.tokenize(s), [])
+          s = '012345'
+          self.assertEqual(self.t.tokenize(s), [])
 
      def test_class_delimiters_string(self):
-      s = 'mother_cleans+the-window'
-      self.assertEqual(self.t.tokenize(s),
+          s = 'mother_cleans+the-window'
+          self.assertEqual(self.t.tokenize(s),
                        ['mother', 'cleans', 'the', 'window'])    
 
 class TestStringMethods(unittest.TestCase):
 
     def test_mother_string(self):
-      s = 'mother cleans the window'
-      self.assertEqual(string_to_alpha0.process_string(s),
+          s = 'mother cleans the window'
+          self.assertEqual(string_to_alpha0.process_string(s),
                        ['mother', 'cleans', 'the', 'window'])
 
     def test_empty_string(self):
-      s = ''
-      self.assertEqual(string_to_alpha0.process_string(s), [])
+          s = ''
+          self.assertEqual(string_to_alpha0.process_string(s), [])
 
     def test_no_spaces_string(self):
-      s = 'mothercleansthewindow'
-      self.assertEqual(string_to_alpha0.process_string(s),
+          s = 'mothercleansthewindow'
+          self.assertEqual(string_to_alpha0.process_string(s),
                        ['mothercleansthewindow'])
 
     def test_digital_string(self):
-      s = '012345'
-      self.assertEqual(string_to_alpha0.process_string(s), [])
+          s = '012345'
+          self.assertEqual(string_to_alpha0.process_string(s), [])
 
     def test_delimiters_string(self):
-      s = 'mother_cleans+the-window'
-      self.assertEqual(string_to_alpha0.process_string(s),
+          s = 'mother_cleans+the-window'
+          self.assertEqual(string_to_alpha0.process_string(s),
                        ['mother', 'cleans', 'the', 'window'])
   
   
