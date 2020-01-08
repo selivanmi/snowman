980a34276bd7379f3eb2633cbf570a69320ed35c
selivanmi <56321315+selivanmi@users.noreply.github.com>
2020-01-08 17:22:24 +0300

    tok0

--- string_to_alpha0.py
+++ string_to_alpha0.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 """This module contains methods and a function
 that tokenizes the given text string"""
 
@@ -55,12 +57,104 @@ class StringTokenizer(object):
            result.append(string[alphaIdx:i])
          return result
 
+    def get_char_type(self, char):
+        if char.isalpha():
+            return TokenType.ALPHA
+        if char.isdigit():
+            return TokenType.NUMBER
+        if char.isspace():
+            return TokenType.SPACE
+        if (',.?!"\':;-()[]'.find(char) > -1):
+            return TokenType.PUNCTUATION
+        return TokenType.UNKNOWN
+            
+    def tokenize_with_token_types(self, string):
+        """
+        This method splits original string into tokens and
+        determines token types
+        @param string: a sequence of characters to be tokenized
+        @return list of objects containing token information - class Token:
+        - Token;
+        - Token type;
+        - First and last indexes in original string
+        """
+        if len(string) == 0:
+            return []
+        result = []
+        i = 0          # Index of the current character
+        cur_token_type = TokenType.UNKNOWN
+        for char in string:
+            if i == 0:    #Initialize variables for first character of string
+                cur_token_type = self.get_char_type(char)
+                begin_idx = 0
+            elif cur_token_type != self.get_char_type(char): #currently analyzed character is of different type
+                    #Add current token to result list and start new token
+                    result.append(Token(string[begin_idx:i], cur_token_type, begin_idx, i))
+                    begin_idx = i
+                    cur_token_type = self.get_char_type(char)
+
+            i += 1
+        #Add last token to result list
+        result.append(Token(string[begin_idx:], cur_token_type, begin_idx, len(string))) 
+        return result
+
+class TokenType(Enum):
+    """
+    Class enumerating token types
+    """
+    UNKNOWN = 0
+    ALPHA = 1
+    NUMBER = 2
+    PUNCTUATION = 3
+    SPACE = 4
+
+class Token(object):
+    """
+    Class representing information about the token:
+    - Substring containing token;
+    - Token type;
+    - First and last indexes in original string
+    """
+
+    def __init__(self, substr, toktype, beg, end):
+        """ Method for Token object initialization """
+        self.token = substr
+        self.token_type = toktype
+        self.begin = beg
+        self.end = end
+
+    def __repr__(self):
+        """ Method for building printable representation of the token """
+        return '\n{Token = "' + self.token + '"; type = ' + self.token_type.name + '; begin = ' + str(self.begin) + '; end = ' + str(self.end) + '}'
+
+    def __eq__(self, other):
+        """
+        Method for token object comparison
+        Two tokens are considered equal
+        only if all token attributes are equal
+        """
+
+        #First of all, classes of compared objects should be equal
+        if isinstance(other, Token):
+            #check if all attributes are equal
+            return (self.token == other.token and
+                    self.token_type == other.token_type and
+                    self.begin == other.begin and
+                    self.end == other.end)
+        #If "other" object is not Token then it can't be equal to Token 
+        return False
+
+
+        
 def main():
     t = StringTokenizer()
-    string = 'mother clears the window'
+    string = 'Mother loves me 2, she clears the window! (joke) Ha-ha!?'
     print(t.tokenize(string))
     result = process_string(string)
     print(result)
+    result = t.tokenize_with_token_types(string)
+    print(result)
+    
 
 if __name__ == '__main__':
     main()
--- testTokenizator_selivanmi.py
+++ testTokenizator_selivanmi.py
@@ -1,8 +1,49 @@
 import unittest
 import string_to_alpha0
 from string_to_alpha0 import StringTokenizer
+from string_to_alpha0 import TokenType
+from string_to_alpha0 import Token
 
+class TestStringTokenizerWithTypes(unittest.TestCase):
 
+     def setUp(self):
+         self.t = StringTokenizer()
+
+     def test_alpha_string(self):
+          s = 'mother cleans the window'
+          self.assertEqual(self.t.tokenize_with_token_types(s)[0],
+                       Token("mother", TokenType.ALPHA, 0, 6))
+          self.assertEqual(len(self.t.tokenize_with_token_types(s)), 7)
+
+     def test_class_empty_string(self):
+          s = ''
+          self.assertEqual(self.t.tokenize(s), [])
+
+     def test_digit_string(self):
+          s = '2 mothers clean 100 windows!!!'
+          self.assertEqual(self.t.tokenize_with_token_types(s)[0],
+                       Token("2", TokenType.NUMBER, 0, 1))
+          self.assertEqual(len(self.t.tokenize_with_token_types(s)), 10)
+
+     def test_punct_string(self):
+          s = '- Can 2 mothers clean 100 windows???'
+          self.assertEqual(self.t.tokenize_with_token_types(s)[0],
+                       Token("-", TokenType.PUNCTUATION, 0, 1))
+          self.assertEqual(len(self.t.tokenize_with_token_types(s)), 14)
+
+     def test_space_string(self):
+          s = ' 2 mothers clean 100 windows???'
+          self.assertEqual(self.t.tokenize_with_token_types(s)[0],
+                       Token(" ", TokenType.SPACE, 0, 1))
+          self.assertEqual(len(self.t.tokenize_with_token_types(s)), 11)
+
+     def test_last_character_string(self):
+          s = '2 mothers clean 100 windows???'
+          self.assertEqual(self.t.tokenize_with_token_types(s)[9],
+                       Token("???", TokenType.PUNCTUATION, 27, 30))
+          self.assertEqual(len(self.t.tokenize_with_token_types(s)), 10)
+
+          
 class TestStringTokenizer(unittest.TestCase):
 
      def setUp(self):
